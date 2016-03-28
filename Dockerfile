b2dc3530b9da7745b38133a381d77ad5d4f5bc03
dlopes7 <davidribeirolopes@gmail.com>
2016-03-28 13:04:01 -0700

    Adding docker-compose definitions

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-FROM django:onbuild
+FROM python:3.5
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /code
+WORKDIR /code
+ADD requirements.txt /code/
+RUN pip install -r requirements.txt
+ADD . /code/
+
--- gothere/settings.py
+++ gothere/settings.py
@@ -81,12 +81,11 @@ WSGI_APPLICATION = 'gothere.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gothere',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'USER': 'gothere',
-        'PASSWORD': 'gothere',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
